46de579ac4ff1e323b773114e67e215955a68592
qwerqwer123412 <127646396+qwerqwer123412@users.noreply.github.com>
2023-03-16 23:23:01 +0900

    Update volume_calc.py
    1. oss_python 1주차 과제입니다
    가로, 세로, 높이를 입력 받은 값을 float형으로 바꿔주고,  입력 받은 값을 전부 곱하여 volume을 구합니다.
    그 후에 print함수를 이용하여 원하는 string을 출력하는 과제입니다.

--- volume_calc.py
+++ volume_calc.py
@@ -1,4 +1,5 @@
 width = float(input("가로:"))
 depth = float(input("세로:"))
 height = float(input("높이:"))
-print(f"박스의 부피는 {width * depth* height}입니다")
+volume = width * depth * height
+print("박스의 부피는", volume, "입니다")
